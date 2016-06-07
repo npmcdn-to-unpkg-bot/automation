12752819dd92dc0bac242a79fc52bd07458726dd
Schappet, James C <james-schappet@uiowa.edu>
2016-06-07 13:41:40 -0500

    Update JSON Date

--- src/main/java/com/schappet/util/JsonDateDeserializer.java
+++ src/main/java/com/schappet/util/JsonDateDeserializer.java
@@ -1,18 +1,22 @@
 package com.schappet.util;
 
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 
 public class JsonDateDeserializer extends JsonDeserializer<Date> {
 	
-    private static final SimpleDateFormat df = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss" );
+    //private static final SimpleDateFormat df = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss" );
+	
+    private static final DateFormat df = new ISO8601DateFormat();
+
     
     	
 	  @Override
--- src/main/java/com/schappet/util/JsonDateSerializer.java
+++ src/main/java/com/schappet/util/JsonDateSerializer.java
@@ -1,7 +1,7 @@
 package com.schappet.util;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
+import java.text.DateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -11,13 +11,14 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 
 @Component
 public class JsonDateSerializer  extends JsonSerializer<Date> {
 
-        private static final SimpleDateFormat df = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ssZ" );
+        //private static final SimpleDateFormat df = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ssZ" );
      
-          
+          private static final DateFormat df = new ISO8601DateFormat();
 
 
 
@@ -32,20 +33,20 @@ public class JsonDateSerializer  extends JsonSerializer<Date> {
              df.setTimeZone( tz );
 
              String output = df.format( date );
-
-             int inset0 = 9;
-             int inset1 = 6;
-             
-             String s0 = output.substring( 0, output.length() - inset0 );
-             String s1 = output.substring( output.length() - inset1, output.length() );
-
-             String result = s0 + s1;
-
-             result = result.replaceAll( "UTC", "+00:00" );
-             
+//
+//             int inset0 = 9;
+//             int inset1 = 6;
+//             
+//             String s0 = output.substring( 0, output.length() - inset0 );
+//             String s1 = output.substring( output.length() - inset1, output.length() );
+//
+//             String result = s0 + s1;
+//
+//             result = result.replaceAll( "UTC", "+00:00" );
+//             
              //return ;
 
-                gen.writeString(result);
+                gen.writeString(output);
         }
 
 
